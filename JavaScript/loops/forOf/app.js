93f079b159bd3a35fb01596ce06588dd24ccfb2e
Rafael Rosa <rafael.f.rosa1@gmail.com>
2022-03-04 08:09:00 -0300

    for..of and for..in loops for iterating

---
+++ JavaScript/loops/forOf/app.js
@@ -0,0 +1,38 @@
+const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics', 'soccer', 'gunners']
+
+// for (let i = 0; i < subreddits.length; i++) {
+//     console.log(`Visit reddit.com/r/${subreddits[i]}`);
+// }
+
+// for (let sub of subreddits) {
+//     console.log(`Visit reddit.com/r/${sub}`);
+// }
+
+////////////////////////////////////////////////////////////////////////
+
+const seatingChart = [
+    ['Kristen', 'Erik', 'Namita'],
+    ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+    ['Yuma', 'Sakura', 'Jack', 'Erika']
+]
+
+// for (let i = 0; i < seatingChart.length; i++) {
+//     const row = seatingChart[i]
+//     console.log(`ROW #${i + 1}`);
+//     for (let j = 0; j < row.length; j++) {
+//         console.log(row[j]);
+//     }
+//     console.log("");
+// }
+
+// for (let row of seatingChart) {
+//     console.log(`ROW #${seatingChart.indexOf(row) + 1}`);
+//     for (let person of row) {
+//         console.log(person);
+//     }
+//     console.log("");
+// }
+
+// for (let char of 'hello world') {
+//     console.log(char);
+// }
