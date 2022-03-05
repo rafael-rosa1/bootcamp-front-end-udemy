ebc0e3d793aa97bf3e8bab977df78e8f605fa2e9
Rafael Rosa <rafael.f.rosa1@gmail.com>
2022-03-05 10:19:00 -0300

    function basics classes

---
+++ JavaScript/functions/basics/app.js
@@ -0,0 +1,51 @@
+// let die1 = Math.floor(Math.random() * 6) + 1;
+// let die2 = Math.floor(Math.random() * 6) + 1;
+// let die3 = Math.floor(Math.random() * 6) + 1;
+// let die4 = Math.floor(Math.random() * 6) + 1;
+// let die5 = Math.floor(Math.random() * 6) + 1;
+// let die6 = Math.floor(Math.random() * 6) + 1;
+
+// let die1 = rollDie(6);
+// let die2 = rollDie(20);
+// let die3 = rollDie(20);
+// let die4 = rollDie(12);
+// let die5 = rollDie();
+// let die6 = rollDie();
+
+function singSong() {
+    console.log("DO");
+    console.log("RE");
+    console.log("MI");
+}
+// //no arguments
+// function greet() {
+//     console.log('Hi!');
+// }
+// //one argument
+// function greet(person) {
+//     console.log(`Hi, ${person}!`)
+// }
+
+//multiple arguments
+function greet(firstName, lastName) {
+    console.log(`Hi, ${firstName} ${lastName[0]}.!`);
+}
+
+function repeat(msg, reps) {
+    let result = '';
+    for (let i = 0; i < reps; i++) {
+        result += msg;
+    }
+    console.log(result);
+}
+
+// singSong();
+// singSong();
+// singSong();
+// singSong();
+// singSong();
+// singSong();
+// singSong();
+// singSong();
+// singSong();
+// singSong();
