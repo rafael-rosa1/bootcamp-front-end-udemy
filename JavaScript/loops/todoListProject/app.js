e15461c6b73c35aca05620f48a236d15652a6ef1
Rafael Rosa <rafael.f.rosa1@gmail.com>
2022-03-04 22:10:56 -0300

    todo list on browser console, did by myself

---
+++ JavaScript/loops/todoListProject/app.js
@@ -0,0 +1,31 @@
+// Todo List 
+// - 'new' - Add a Todo
+// - 'list' - list all todos
+// - 'delete' - remove specific todo
+// - 'quit' - quit app
+
+let command = prompt('What you would like to do?');
+let todoList = [];
+
+while (command !== 'quit') {
+    if (command === 'new') {
+        let newTodo = prompt('Insert a new todo.');
+        todoList.push(newTodo);
+        console.log(`${newTodo} added to the list`);
+    } else if (command === 'list') {
+        console.log('*********************');
+        for (todo of todoList) {
+            console.log(`${todoList.indexOf(todo)}: ${todo}`);
+        }
+        console.log('*********************');
+    } else if (command === 'delete') {
+        let deleteIndex = prompt('Choose a todo to remove');
+        console.log(`${todoList[deleteIndex]} removed`);
+        todoList.splice(deleteIndex, 1);
+    } else {
+        command = prompt('What you would like to do?');
+    }
+    command = prompt('What you would like to do?');
+}
+
+console.log(`OK, YOU QUIT THE APP`);
