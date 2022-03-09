57517ee7535356de687f377e8f0ffa4a0dd3ac8b
Rafael Rosa <rafael.f.rosa1@gmail.com>
2022-03-09 20:26:38 -0300

    learning arrow functions

---
+++ JavaScript/arrowFunctions/app.js
@@ -0,0 +1,19 @@
+// const add = function (x, y) {
+//     return x + y;
+// }
+
+// function add(x, y) {
+//     return x + y;
+// }
+
+const add = (x, y) => {
+    return x + y;
+}
+
+const square = (x) => {
+    return x * x;
+}
+
+const rollDie = () => {
+    return Math.floor(Math.random() * 6) + 1;
+}
