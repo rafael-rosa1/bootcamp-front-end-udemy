7bb84705cf2e5847572e4200dae6c953ad1c8346
Rafael Rosa <rafael.f.rosa1@gmail.com>
2022-03-09 22:24:27 -0300

    using arrow functions on array methods

--- JavaScript/arrowFunctions/app.js
+++ JavaScript/arrowFunctions/app.js
@@ -19,9 +19,37 @@ const square = (x) => {
 // }
 
 // implicit returns: using return without typing it
+// only works if there is only one statement/expressions on the body of a function
 const rollDie = () => (
     Math.floor(Math.random() * 6) + 1
 )
 
 //usefull for making oneline functions
 const add = (a, b) => a + b;
+
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 99
+    },
+    {
+        title: 'Stand By Me',
+        score: 85
+    },
+    {
+        title: 'Parasite',
+        score: 95
+    },
+    {
+        title: 'Alien',
+        score: 90
+    }
+]
+
+// const newMovies = movies.map(function (movie) {
+//     return `${movie.title} - ${movie.score / 10}`;
+// })
+
+const newMovies = movies.map(movie => (
+    `${movie.title} - ${movie.score / 10}`)
+)
