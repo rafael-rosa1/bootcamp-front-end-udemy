71349531b6ffb5c38d119f4b0cd934807d63efe4
Rafael Rosa <rafael.f.rosa1@gmail.com>
2022-03-09 22:41:17 -0300

    setTimeout and setInterval

---
+++ JavaScript/setTimeout/app.js
@@ -0,0 +1,10 @@
+// console.log('HELLO!!');
+// setTimeout(() => {
+//     console.log('...are you still there?');
+// }, 3000)
+
+const id = setInterval(() => {
+    console.log(Math.random())
+}, 2000);
+
+// clearInterval(id);
