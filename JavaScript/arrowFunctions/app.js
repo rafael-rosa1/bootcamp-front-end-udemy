1de1ac79684770b500dd041eb53f3634b3546d60
Rafael Rosa <rafael.f.rosa1@gmail.com>
2022-03-09 22:15:27 -0300

    implicit returns

--- JavaScript/arrowFunctions/app.js
+++ JavaScript/arrowFunctions/app.js
@@ -6,14 +6,22 @@
 //     return x + y;
 // }
 
-const add = (x, y) => {
-    return x + y;
-}
+// const add = (x, y) => {
+//     return x + y;
+// }
 
 const square = (x) => {
     return x * x;
 }
 
-const rollDie = () => {
-    return Math.floor(Math.random() * 6) + 1;
-}
+// const rollDie = () => {
+//     return Math.floor(Math.random() * 6) + 1;
+// }
+
+// implicit returns: using return without typing it
+const rollDie = () => (
+    Math.floor(Math.random() * 6) + 1
+)
+
+//usefull for making oneline functions
+const add = (a, b) => a + b;
