e0efd0f2731abe8659c2203f65b6b36769dbc608
Rafael Rosa <rafael.f.rosa1@gmail.com>
2022-03-22 06:25:45 -0300

    event and change events

---
+++ DOMEvents/ChangeAndInputEvents/app.js
@@ -0,0 +1,8 @@
+const input = document.querySelector('input');
+const h1 = document.querySelector('h1');
+// input.addEventListener('change', function(e){
+//   console.log('CHANGE EVENT');
+// })
+input.addEventListener('input', (e) => {
+  h1.innerText = input.value;
+})
