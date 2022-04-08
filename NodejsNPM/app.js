c5746f062fb5bb2a0e6bcf673d55443b2aa85482
Rafael Rosa <rafael.f.rosa1@gmail.com>
2022-04-07 22:14:53 -0300

    working with module.exports

---
+++ NodejsNPM/app.js
@@ -0,0 +1,4 @@
+const { PI, square, add } = require('./math');
+console.log(PI);
+console.log(square(9));
+console.log(add(10, 3));
---
+++ NodejsNPM/math.js
@@ -0,0 +1,20 @@
+const add = (x, y) => x + y;
+
+const PI = 3.14159;
+
+const square = (x) => x * x;
+
+exports.square = square;
+exports.PI = PI;
+exports.add = add;
+
+// const math = {
+// 	add: add,
+// 	PI: PI,
+// 	square: square
+// };
+// module.exports = math;
+
+// module.exports.add = add;
+// module.exports.PI = PI;
+// module.exports.square = square;
