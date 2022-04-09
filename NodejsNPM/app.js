93c92a0dec7cd0801608538dd96015f3cabbecc5
Rafael Rosa <rafael.f.rosa1@gmail.com>
2022-04-08 22:44:43 -0300

    importing/exporting an entire folder

--- NodejsNPM/app.js
+++ NodejsNPM/app.js
@@ -1,4 +1,7 @@
 const { PI, square, add } = require('./math');
-console.log(PI);
-console.log(square(9));
-console.log(add(10, 3));
+// console.log(PI);
+// console.log(square(9));
+// console.log(add(10, 3));
+
+const cats = require('./shelter');
+console.log('required an entire directory!', cats);
