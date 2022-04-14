75894df0eafd85bb62076fbe5e938cb2c7672ead
Rafael Rosa <rafael.f.rosa1@gmail.com>
2022-04-13 22:11:45 -0300

    using express and template together

--- Express/Templating/index.js
+++ Express/Templating/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const redditData = require('./data.json');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
@@ -9,9 +10,19 @@ app.get('/', (req, res) => {
 	res.render('home.ejs');
 });
 
+app.get('/cats', (req, res) => {
+	const cats = [ 'Blue', 'Monty', 'Ronald', 'Gaby', 'Rocket' ];
+	res.render('cats', { cats });
+});
+
 app.get('/r/:subreddit', (req, res) => {
 	const { subreddit } = req.params;
-	res.render('subreddit', { subreddit });
+	const data = redditData[subreddit];
+	if (data) {
+		res.render('subreddit', { ...data });
+	} else {
+		res.render('notfound', { subreddit });
+	}
 });
 
 app.get('/rand', (req, res) => {
