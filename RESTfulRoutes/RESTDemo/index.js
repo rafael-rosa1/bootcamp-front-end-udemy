a2ce913ed2e55dc9cd13fd516c5a4677912b0155
Rafael Rosa <rafael.f.rosa1@gmail.com>
2022-04-18 20:50:16 -0300

    RESTfull starting

--- RESTfulRoutes/RESTDemo/index.js
+++ RESTfulRoutes/RESTDemo/index.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const comments = [
+	{
+		username: 'Todd',
+		comment: 'lol that is so funny'
+	},
+	{
+		username: 'Skyler',
+		comment: 'i like to go birdwatching with my dog'
+	},
+	{
+		username: 'Sk8erBoi',
+		comment: 'plz delete your account, Todd'
+	},
+	{
+		username: 'onlysayswoof',
+		comment: 'woof woof woof'
+	}
+];
+
+app.get('/comments', (req, res) => {
+	res.render('comments/index', { comments });
+});
 
 app.get('/tacos', (req, res) => {
 	res.send('GET /tacos response');
@@ -16,3 +42,9 @@ app.post('/tacos', (req, res) => {
 app.listen(3000, () => {
 	console.log('ON PORT 3000!');
 });
+
+// GET /comments - list all comments
+// POST /comments - create a new comment
+// GET /comments/:id - Get one comment (using ID)
+// PATCH /comments/:id - Update one comment
+// DELETE /comments/id - Destroy one comment
