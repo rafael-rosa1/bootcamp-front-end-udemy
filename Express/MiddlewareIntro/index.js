e20fe0a504cca13883dbb9da976ddc1252606fc3
Rafael Rosa <rafael.f.rosa1@gmail.com>
2022-04-28 23:20:20 -0300

    learning about middleware

---
+++ Express/MiddlewareIntro/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+	req.requestTime = Date.now();
+	console.log(req.method, req.path);
+	next();
+});
+// app.use((req, res, next) => {
+// 	console.log('This is my FIRST middleware');
+// 	return next(); //without next the code stops here, we dont see the second message on the console
+// 	console.log('This is my FIRST middleware - after calling next()');
+// });
+// app.use((req, res, next) => {
+// 	console.log('This is my SECOND middleware');
+// 	return next();
+// });
+// app.use((req, res, next) => {
+// 	console.log('This is my THIRD middleware');
+// 	return next();
+// });
+
+app.get('/', (req, res) => {
+	console.log(`REQUEST DATE: ${req.requestTime}`);
+	res.send('homepage');
+});
+
+app.get('/dogs', (req, res) => {
+	res.send('WOOF WOOF');
+});
+
+app.listen(3000, () => {
+	console.log('App is running on localhost:3000');
+});
