a298656085ec78a974424812d49697f5d37d9142
Rafael Rosa <rafael.f.rosa1@gmail.com>
2022-05-07 12:29:32 -0300

    little correction

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -38,7 +38,7 @@ app.get('/campgrounds/new', (req, res) => {
 });
 
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
-  if (!req, body.campground) throw new ExpressError('Invalid Campground Data', 400);
+  if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
   const campground = new Campground(req.body.campground);
   await campground.save();
   res.redirect(`/campgrounds/${campground._id}`);
