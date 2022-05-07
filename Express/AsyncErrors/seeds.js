25f0f61c8511aec603d8a7c750376496f525b5a9
Rafael Rosa <rafael.f.rosa1@gmail.com>
2022-05-07 12:23:06 -0300

    validation and error handling

--- Express/AsyncErrors/seeds.js
+++ Express/AsyncErrors/seeds.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Product = require('./models/product');
+const Product = require("./models/product");
 
 mongoose
-	.connect('mongodb://localhost:27017/farmStand2')
-	.then(() => {
-		console.log('MONGO CONNECTION OPEN');
-	})
-	.catch((err) => {
-		console.log('OH NO MONGO CONNECTION ERROR !!');
-		console.log(err);
-	});
+  .connect("mongodb://localhost:27017/farmStand2")
+  .then(() => {
+    console.log("MONGO CONNECTION OPEN");
+  })
+  .catch((err) => {
+    console.log("OH NO MONGO CONNECTION ERROR !!");
+    console.log(err);
+  });
 
 // const p = new Product({
 // 	name: 'Ruby Grapefruit',
@@ -27,37 +27,37 @@ mongoose
 // 	});
 
 const seedProducts = [
-	{
-		name: 'Fairy Eggplant',
-		price: 1.0,
-		category: 'vegetable'
-	},
-	{
-		name: 'Organic Goddess Melon',
-		price: 4.99,
-		category: 'fruit'
-	},
-	{
-		name: 'Organic Mini Seedless Watermelon',
-		price: 3.99,
-		category: 'fruit'
-	},
-	{
-		name: 'Organic Celery',
-		price: 1.5,
-		category: 'vegetable'
-	},
-	{
-		name: 'Chocolate Whole Milk',
-		price: 2.69,
-		category: 'dairy'
-	}
+  {
+    name: "Fairy Eggplant",
+    price: 1.0,
+    category: "vegetable",
+  },
+  {
+    name: "Organic Goddess Melon",
+    price: 4.99,
+    category: "fruit",
+  },
+  {
+    name: "Organic Mini Seedless Watermelon",
+    price: 3.99,
+    category: "fruit",
+  },
+  {
+    name: "Organic Celery",
+    price: 1.5,
+    category: "vegetable",
+  },
+  {
+    name: "Chocolate Whole Milk",
+    price: 2.69,
+    category: "dairy",
+  },
 ];
 
 Product.insertMany(seedProducts)
-	.then((res) => {
-		console.log(res);
-	})
-	.catch((e) => {
-		console.log(e);
-	});
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
