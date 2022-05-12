e8c2d7d375fe8a65757cb4b6b75135221ceec85e
Rafael Rosa <rafael.f.rosa1@gmail.com>
2022-05-11 22:21:50 -0300

    mongo relationships - one to few

---
+++ MongoRelationships/Models/user.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/relationshipDemo');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
+});
+
+const userSchema = new mongoose.Schema({
+    first: String,
+    last: String,
+    addresses: [
+        {
+            street: String,
+            city: String,
+            state: String,
+            country: String
+        }
+    ]
+});
+
+const User = mongoose.model('User', userSchema);
+
+const makeUser = async () => {
+    const u = new User({
+        first: 'Valentina',
+        last: 'Rosa'
+    });
+    u.addresses.push({
+        street: '123 Sesame St',
+        city: 'New York',
+        state: 'NY',
+        country: 'USA'
+    });
+    const res = await u.save();
+    console.log(res);
+};
+
+const addAddress = async (id) => {
+    const user = await User.findById(id);
+    user.addresses.push({
+        street: '99 3rd St.',
+        city: 'New York',
+        state: 'NY',
+        country: 'USA'
+    });
+    const res = await user.save();
+    console.log(res);
+}
+
+addAddress('627c5f96f20c2a7a66450646');
