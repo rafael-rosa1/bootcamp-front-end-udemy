74b6bde255b981ce1fcc429b1211743cc3e839a0
Rafael Rosa <rafael.f.rosa1@gmail.com>
2022-05-18 22:24:52 -0300

    mongo relationships the end

--- MongoRelWithExpress/index.js
+++ MongoRelWithExpress/index.js
@@ -34,9 +34,14 @@ app.get('/farms/new', (req, res) => {
 });
 
 app.get('/farms/:id', async (req, res) => {
-	const farm = await Farm.findById(req.params.id);
+	const farm = await Farm.findById(req.params.id).populate('products');
 	res.render('farms/show', { farm });
 });
+
+app.delete('/farms/:id', async (req, res) => {
+	const farm = await Farm.findByIdAndDelete(req.params.id);
+	res.redirect('/farms');
+})
 
 app.post('/farms', async (req, res) => {
 	const farm = new Farm(req.body);
@@ -44,9 +49,10 @@ app.post('/farms', async (req, res) => {
 	res.redirect('/farms');
 });
 
-app.get('/farms/:id/products/new', (req, res) => {
+app.get('/farms/:id/products/new', async (req, res) => {
 	const { id } = req.params;
-	res.render('products/new', { categories, id });
+	const farm = await Farm.findById(id);
+	res.render('products/new', { categories, farm });
 });
 
 app.post('/farms/:id/products', async (req, res) => {
@@ -58,7 +64,7 @@ app.post('/farms/:id/products', async (req, res) => {
 	product.farm = farm;
 	await farm.save();
 	await product.save();
-	res.send(farm);
+	res.redirect(`/farms/${id}`);
 
 });
 
@@ -89,7 +95,7 @@ app.post('/products', async (req, res) => {
 
 app.get('/products/:id', async (req, res) => {
 	const { id } = req.params;
-	const product = await Product.findById(id);
+	const product = await Product.findById(id).populate('farm', 'name');
 	res.render('products/show', { product });
 });
 
