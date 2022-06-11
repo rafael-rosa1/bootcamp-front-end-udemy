092eb1073dc5adbe76e684026886e52c3249a1ba
Rafael Rosa <rafael.f.rosa1@gmail.com>
2022-06-11 10:15:54 -0300

    deleting images and adding new ones

--- YelpCamp/controllers/campgrounds.js
+++ YelpCamp/controllers/campgrounds.js
@@ -1,4 +1,5 @@
 const Campground = require('../models/campground');
+const { cloudinary } = require('../cloudinary');
 
 module.exports.index = async (req, res) => {
 	const campgrounds = await Campground.find({});
@@ -14,7 +15,6 @@ module.exports.createCampground = async (req, res, next) => {
 	campground.images = req.files.map((f) => ({ url: f.path, filename: f.filename }));
 	campground.author = req.user._id;
 	await campground.save();
-	console.log(campground);
 	req.flash('success', 'Sucessfully made a new campground!');
 	res.redirect(`/campgrounds/${campground._id}`);
 };
@@ -28,7 +28,6 @@ module.exports.showCampground = async (req, res) => {
 			}
 		})
 		.populate('author');
-	console.log(campground);
 	if (!campground) {
 		req.flash('error', 'Cannot find that campground!');
 		res.redirect('/campgrounds');
@@ -50,12 +49,20 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.updateCampground = async (req, res) => {
 	const { id } = req.params;
+	console.log(req.body);
 	const campground = await Campground.findByIdAndUpdate(id, {
 		...req.body.campground
 	});
 	const imgs = req.files.map((f) => ({ url: f.path, filename: f.filename }));
 	campground.images.push(...imgs);
 	await campground.save();
+	if (req.body.deleteImages) {
+		for (let filename of req.body.deleteImages) {
+			await cloudinary.uploader.destroy(filename);
+		}
+		await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } });
+		console.log(campground);
+	}
 	req.flash('success', 'Successfully updated campground!');
 	res.redirect(`/campgrounds/${campground._id}`);
 };
